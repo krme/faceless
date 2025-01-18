bbec6e3e3ac2e15cdc06b43a944d82412d1f64b4
fred-codesphere <>
2025-01-18 14:26:53 +0100

    feature: add random sentence creator

---
+++ jobs/flows/createSentence.py
@@ -0,0 +1,30 @@
+
+from mistralai import Mistral
+
+def createSentence() -> str:
+    api_key = "rNQf5SkjXzuEbKHMjRGdsmgWlBLODXhz"
+    model = "mistral-small-latest"
+
+    client = Mistral(api_key=api_key)
+
+    chat_response = client.chat.complete(
+        model = model,
+        messages = [
+            {
+                "role": "user",
+                "top_p": 0.9,
+                "content": "Create 1 absurdly creative, very, very, very short sentence with no constraints on logic or topic in simple English and nothing else, no nothing. without sure or anything.",
+            },
+        ]
+    )
+
+    print(chat_response.choices[0].message.content)
+    return chat_response.choices[0].message.content
+
+
+def main():
+    createSentence()
+
+
+if __name__ == "__main__":
+    main()
